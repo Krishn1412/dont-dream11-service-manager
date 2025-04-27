a8dd0cc6f6189247118aaf78c0b991cfa97e0db1
Krishn Parasar <76171905+Krishn1412@users.noreply.github.com>
2025-04-28 01:33:22 +0530

    Furthering the folder structure
    Still thinking about the idea of how this service will look like.

---
+++ core/game_registry.py
@@ -0,0 +1,36 @@
+class GameRegistry:
+    def __init__(self):
+        # Store games here: {game_id: {market_name: market_data}}
+        self.games = {}
+
+    def register_game(self, game_id):
+        if game_id not in self.games:
+            self.games[game_id] = {}
+    
+    def add_market(self, game_id, market_name, initial_odds=None):
+        self.register_game(game_id)
+        if market_name not in self.games[game_id]:
+            self.games[game_id][market_name] = {
+                "odds": initial_odds,
+                "bets": [],
+            }
+    
+    def update_odds(self, game_id, market_name, new_odds):
+        if game_id in self.games and market_name in self.games[game_id]:
+            self.games[game_id][market_name]["odds"] = new_odds
+
+    def add_bet(self, game_id, market_name, bet_data):
+        if game_id in self.games and market_name in self.games[game_id]:
+            self.games[game_id][market_name]["bets"].append(bet_data)
+    
+    def get_market_info(self, game_id, market_name):
+        return self.games.get(game_id, {}).get(market_name, None)
+    
+    def get_all_games(self):
+        return self.games
+
+    def game_exists(self, game_id):
+        return game_id in self.games
+
+    def market_exists(self, game_id, market_name):
+        return self.game_exists(game_id) and market_name in self.games[game_id]
